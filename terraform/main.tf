1aa09bb74751515f9c09f7d06cbeee7cfe204f13
osawa-koki <github@osawa.dev>
2023-02-25 14:41:35 +0900

    リソースグループ消した。

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,3 @@
 provider "aws" {
   region = var.region
 }
-
-resource "aws_resourcegroups_group" "resource_group" {
-  name = "my-${var.project_name}-resource-group"
-  tags = {
-    Name        = "${var.project_name}-resource-group"
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
