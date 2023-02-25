38479a29aaf722870a0fdf8d6cb0f6f0151a8e12
osawa-koki <github@osawa.dev>
2023-02-25 16:16:52 +0900

    小さな修正。

--- terraform/data.tf
+++ terraform/data.tf
@@ -8,7 +8,6 @@ resource "aws_db_instance" "postgresql" {
   db_name             = var.db_name
   username            = var.db_username
   password            = var.db_password
-  timezone            = "Tokyo Standard Time"
   publicly_accessible = true
   skip_final_snapshot = false
 
--- terraform/network.tf
+++ terraform/network.tf
@@ -2,6 +2,8 @@
 # VPCを作成
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "${var.project_name}-vpc"
   }
