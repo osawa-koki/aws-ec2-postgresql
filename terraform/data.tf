c6d66c3a2488df38b5d6eb8956a611bd3559bddb
osawa-koki <github@osawa.dev>
2023-02-25 15:33:38 +0900

    DBへのパブリックアクセスを許可。

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,14 +1,15 @@
 
 # PostgreSQLデータベースを作成
 resource "aws_db_instance" "postgresql" {
-  allocated_storage = 10
-  engine            = "postgres"
-  engine_version    = "11.12"
-  instance_class    = "db.t2.micro"
-  db_name           = var.db_name
-  username          = var.db_username
-  password          = var.db_password
-  timezone          = "Tokyo Standard Time"
+  allocated_storage   = 10
+  engine              = "postgres"
+  engine_version      = "11.12"
+  instance_class      = "db.t2.micro"
+  db_name             = var.db_name
+  username            = var.db_username
+  password            = var.db_password
+  timezone            = "Tokyo Standard Time"
+  publicly_accessible = true
 
   # VPCへの関連
   db_subnet_group_name = aws_db_subnet_group.subnet_group.name
