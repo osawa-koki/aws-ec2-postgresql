0ab9ae4cb23cec714843e2b258d7bfaed4eee1d1
osawa-koki <github@osawa.dev>
2023-02-25 14:59:15 +0900

    IGW

--- terraform/network.tf
+++ terraform/network.tf
@@ -80,3 +80,26 @@ resource "aws_security_group" "security_group" {
     Name = "${var.project_name}-security-group"
   }
 }
+
+# インターネットゲートウェイを作成する
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# パブリックサブネットのルートテーブルを作成する
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# パブリックサブネットにルートテーブルを関連付ける
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# パブリックルートを作成する
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
