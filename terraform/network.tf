12751892b06b0864ea0dc745dc5dcbe1738c45d7
osawa-koki <github@osawa.dev>
2023-02-25 14:18:06 +0900

    AZ

--- terraform/network.tf
+++ terraform/network.tf
@@ -19,11 +19,13 @@ resource "aws_subnet" "subnet" {
 }
 resource "aws_subnet" "subnet_a" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
 }
 resource "aws_subnet" "subnet_b" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-1c"
 }
 
 # VPCに関する設定
