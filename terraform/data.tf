8f4f6e9dd0d8dc3ccbdc701529a37373fcb0de9f
osawa-koki <github@osawa.dev>
2023-02-25 15:28:25 +0900

    小さな修正。

--- terraform/data.tf
+++ terraform/data.tf
@@ -8,6 +8,7 @@ resource "aws_db_instance" "postgresql" {
   db_name           = var.db_name
   username          = var.db_username
   password          = var.db_password
+  timezone          = "Tokyo Standard Time"
 
   # VPCへの関連
   db_subnet_group_name = aws_db_subnet_group.subnet_group.name
