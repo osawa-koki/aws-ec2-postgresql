a6a47b95cba6b76cdf339afbb9ba2306f80cd1bc
osawa-koki <github@osawa.dev>
2023-02-25 14:26:13 +0900

    リソースグループ。

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,12 @@
 provider "aws" {
   region = var.region
 }
+
+resource "aws_resourcegroups_group" "resource_group" {
+  name = "${var.project_name}-resource-group"
+  tags = {
+    Name        = "${var.project_name}-resource-group"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
